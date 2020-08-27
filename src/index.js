0be903ebcec5a40d85f7914cc279df444bba39f3
Kelly He <kellyhe@mit.edu>
2020-08-27 17:48:06 -0400

    nice

--- src/index.js
+++ src/index.js
@@ -62,11 +62,7 @@ const theme = createMuiTheme({
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-<<<<<<< HEAD
     <Router basename={process.env.PUBLIC_URL}>
-=======
-    <Router>
->>>>>>> 927056aa34585b06f842d9db9d94367b2b8a3eec
       <App />
     </Router>
   </MuiThemeProvider>,
